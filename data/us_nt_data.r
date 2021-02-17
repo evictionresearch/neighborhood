c636a672fdbba4853f786b44c1378a70c71209ea
Tim Thomas <timothyathomas@gmail.com>
2021-02-17 10:19:50 -0800

    adding us nt data

--- data/us_nt_data.r
+++ data/us_nt_data.r
@@ -4,16 +4,25 @@
 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load_gh("timathomas/neighborhood")
-pacman::p_load(sf, tigris, tidyverse)
+pacman::p_load(sf, tigris, tidycensus, tidyverse)
+options(tigris_use_cache = TRUE)
 
 # ==========================================================================
-# Create data
+# Create and save data
 # ==========================================================================
 
 us_states <- 
 	states() %>% 
 	st_set_geometry(NULL) %>% 
-	pull(STATEFP)
+  filter(!STUSPS %in% c("AS", "GU", "MP", "VI")) %>% 
+	pull(STUSPS, ) %>% 
+  sort()
 
-us_tracts <- 
-	ntdf(state = us_states)
+us_tracts <-
+  map_df(us_states, function(states){
+    bind_rows(
+  	  ntdf(state = states, year = 2019)
+  	  )
+    })
+
+saveRDS(us_tracts, "~/git/neighborhood/data/us_nt_tracts.rds")
