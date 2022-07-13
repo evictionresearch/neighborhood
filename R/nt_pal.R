93acfeebcaaac20a63874ded0d4edf7cf9ee1f2f
Tim Thomas <timthomas@berkeley.edu>
2022-07-13 15:58:36 -0600

    Update Latino

--- R/nt_pal.R
+++ R/nt_pal.R
@@ -9,18 +9,18 @@ nt_pal <- function(df, ...){
   leaflet::colorFactor(c(
     '#33a02c', # 'Mostly Asian', green
     '#1f78b4', # 'Mostly Black', blue
-    '#e31a1c', # 'Mostly Latinx', red
+    '#e31a1c', # 'Mostly Latino', red
     '#9b66b0', # 'Mostly Other', purple
     '#C95123', # 'Mostly White',
     '#1fc2ba', # 'Asian-Black',
-    '#d6ae5c', # 'Asian-Latinx',
+    '#d6ae5c', # 'Asian-Latino',
     '#91c7b9', # 'Asian-Other',
     '#b2df8a', # 'Asian-White',
-    '#de4e4b', # 'Black-Latinx',
+    '#de4e4b', # 'Black-Latino',
     '#71a1f5', # 'Black-Other',
     '#a6cee3', # 'Black-White',
-    '#f0739b', # 'Latinx-Other',
-    '#fb9a99', # 'Latinx-White',
+    '#f0739b', # 'Latino-Other',
+    '#fb9a99', # 'Latino-White',
     '#c28a86', # 'Other-White',
     '#fdbf6f', # '3 Group Mixed',
     '#cab2d6', # '4 Group Mixed',
