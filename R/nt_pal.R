20a2f8b2b9d5808c296a161dd2837578fb28b293
Tim Thomas <timthomas@berkeley.edu>
2022-10-06 13:04:32 -0700

    change to latine

--- R/nt_pal.R
+++ R/nt_pal.R
@@ -9,18 +9,18 @@ nt_pal <- function(df, ...){
   leaflet::colorFactor(c(
     '#33a02c', # 'Mostly Asian', green
     '#1f78b4', # 'Mostly Black', blue
-    '#e31a1c', # 'Mostly Latino', red
+    '#e31a1c', # 'Mostly Latine', red
     '#9b66b0', # 'Mostly Other', purple
     '#C95123', # 'Mostly White',
     '#1fc2ba', # 'Asian-Black',
-    '#d6ae5c', # 'Asian-Latino',
+    '#d6ae5c', # 'Asian-Latine',
     '#91c7b9', # 'Asian-Other',
     '#b2df8a', # 'Asian-White',
-    '#de4e4b', # 'Black-Latino',
+    '#de4e4b', # 'Black-Latine',
     '#71a1f5', # 'Black-Other',
     '#a6cee3', # 'Black-White',
-    '#f0739b', # 'Latino-Other',
-    '#fb9a99', # 'Latino-White',
+    '#f0739b', # 'Latine-Other',
+    '#fb9a99', # 'Latine-White',
     '#c28a86', # 'Other-White',
     '#fdbf6f', # '3 Group Mixed',
     '#cab2d6', # '4 Group Mixed',
